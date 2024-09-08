bde1ee8e1f8edb94367c3b988f12c88dfd6d51c7
maphuti2 <maphuti22r@gmail.com>
2024-09-08 20:38:40 +0200

    Writing code that outputs reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(name) {
+    let sentence = '';
+    for (let x = name.length -1; x >= 0; x--){
+        sentence += name[x];
+    }
+    return sentence;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
