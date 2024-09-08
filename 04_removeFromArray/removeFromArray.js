1a340cd1f73a35483194dc486e4c1f3de39c16e7
maphuti2 <maphuti22r@gmail.com>
2024-09-08 21:40:08 +0200

    Writing function that removes all similar array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array , ...arg) {
+    let fixedArray = [];
+    for (let x of array){
+        if (!arg.includes(x)){
+            fixedArray.push(x);
+        }
+    }
+
+    return fixedArray;
 };
 
 // Do not edit below this line
