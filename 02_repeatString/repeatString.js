2dfe1a47ed1ab48c4da73a9be8832f137123c24c
maphuti2 <maphuti22r@gmail.com>
2024-09-08 10:41:23 +0200

    Writing a function that accepts string and a number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(name, number) {
+     let word = '';
+     if (number < 0){
+        word = 'ERROR';
+     }
+    for (let x = 0; x < number; x++){
+        word += name;
+    }
 
+    return word;
 };
 
 // Do not edit below this line
