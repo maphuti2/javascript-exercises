20d1804e0226748a36d110f334262a1025115c88
maphuti2 <maphuti22r@gmail.com>
2024-09-09 09:55:03 +0200

    I loved the challenges, they were very educational

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const century = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
+    if (divisibleByFour && !century || divisibleByFourHundred){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const degrees = (5/9) * (temp - 32);
+  if (degrees < 0){
+    return Math.round(degrees * 10) / 10;
+  }
+  return Math.round(degrees * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahr = (temp * 1.8) + 32;
+  if (fahr < 0){
+    return Math.round(fahr * 10) / 10;
+  }
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
