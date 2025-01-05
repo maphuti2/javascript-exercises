521ac4e680115f2dbe10e51a02db77af83f4ee27
maphuti2 <maphuti22r@gmail.com>
2025-01-05 15:37:38 +0200

    fibonacci number complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const valueNum = Number(n);
+    let numbers = [1,1];
 
+
+    if (valueNum === 0){
+        return valueNum;
+    }
+
+    if (valueNum < 0){
+        return `OOPS`;
+    }
+
+    for (let x = 2; x < valueNum; x++){
+        let nextNum = numbers[x - 1] + numbers[x - 2];
+        numbers.push(nextNum);
+    }
+
+    return numbers[valueNum - 1];
 };
 
 // Do not edit below this line
