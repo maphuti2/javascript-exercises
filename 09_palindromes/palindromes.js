7fb7e7ca2fbc5d39020e16e2e8701bd706dd5117
maphuti2 <maphuti22r@gmail.com>
2025-01-05 14:20:46 +0200

    Palindromes exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const originWord = words.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedWord = originWord.split('').reverse().join('');
 
+    return originWord === reversedWord;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
