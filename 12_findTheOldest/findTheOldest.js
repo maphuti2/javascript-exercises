5386bb45dcee5bd0d1b6c957944e00e622107777
maphuti2 <maphuti22r@gmail.com>
2025-01-05 17:16:15 +0200

    Oldest years

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
+function findTheOldest(people) { 
+    const stillLive = new Date().getFullYear();
+    return people.reduce((oldestPerson, currentPerson) => { 
+        const oldestAge = (oldestPerson.yearOfDeath || stillLive ) - oldestPerson.yearOfBirth; const currentAge = (currentPerson.yearOfDeath || stillLive) - currentPerson.yearOfBirth; 
+        
+        return currentAge > oldestAge ? currentPerson : oldestPerson; });
 
-};
 
+    }
 // Do not edit below this line
 module.exports = findTheOldest;
